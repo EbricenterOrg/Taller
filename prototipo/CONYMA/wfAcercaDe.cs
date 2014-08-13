605831e252a0915562a9f0d5fbedb1b6b4c6d034
rails -v <josambro@hotmail.com>
2014-08-13 17:08:28 -0600

    Modulo de seguridad
    inicio de sesion de los usuarios

---
+++ prototipo/CONYMA/wfAcercaDe.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace EBRICENTER
+{
+    partial class wfAcercaDe : Form
+    {
+        public wfAcercaDe()
+        {
+            InitializeComponent();
+            this.Text = String.Format("Acerca de {0}", AssemblyTitle);
+            this.lblNomProducto.Text = AssemblyProduct;
+            this.lblVersion.Text = String.Format("Versión {0}", AssemblyVersion);
+            this.lblCopyright.Text = AssemblyCopyright;
+            this.lblComName.Text = AssemblyCompany;
+            this.txtDescription.Text = AssemblyDescription;
+        }
+
+        #region Descriptores de acceso de atributos de ensamblado
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+
+        private void wfAcercaDe_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelProductName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ prototipo/CONYMA/wfMenuPrincipal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EBRICENTER {
+	public partial class wfMenuPrincipal : Form {
+
+
+
+        private string usuario;
+
+
+		
+		public wfMenuPrincipal(string sUsuario) {
+            usuario = sUsuario;
+			InitializeComponent();
+           
+		}
+
+        private void iniciarConexion()
+        {
+          
+
+
+        }
+
+		
+		void WfMenuLoad(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "Conectado..." +usuario;
+		}
+		
+		void SalirToolStripMenuItemClick(object sender, EventArgs e) {
+			this.Close();
+		}
+
+        private void btninfo_Click(object sender, EventArgs e)
+        {
+                     
+            wfAcercaDe info = new wfAcercaDe();
+            info.MdiParent = this;
+            info.Show();
+            
+
+        }
+
+        private void btnNu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+       
+
+        
+	}
+}
